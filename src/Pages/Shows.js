747ead07fa67bc831783c7ecb98260fdecd919ea
Ian Miller 🦐 <ianmiller347@gmail.com>
2021-01-18 00:45:19 -0500

    add spotify and twitch embed to shows page

--- src/Pages/Shows.js
+++ src/Pages/Shows.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { TwitchEmbed, TwitchChat } from 'react-twitch-embed';
 
-class Shows extends React.Component {
-  render() {
-    return (
-      <> 
-        <h2>No shows booked right now :(</h2>
-      </>
-    );
-  }
-}
+const Shows = () => {
+  return (
+    <> 
+      <h2>No live shows booked right now :(</h2>
+      <p>Instead check out friendshrimp on <a href="https://twitch.tv/friendshrimps">Twitch</a> or Spotify!</p>
+      <div className="margin-bottom-s">
+        <iframe
+          src="https://open.spotify.com/embed/track/3fYSFITHV36HCLexVWaUJd"
+          width="300"
+          height="380"
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        />
+      </div>
+      <div className="twitch">
+        <TwitchEmbed
+          channel="friendshrimps"
+          id="friendshrimps"
+          className="margin-center"
+          theme="dark"
+          muted
+          onVideoPause={() => console.log('paused video :(')}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Shows;
