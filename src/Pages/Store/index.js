6dec19064edb4323fc83aedc0cba0de213331389
Ian Miller 🦐 <ianmiller347@gmail.com>
2021-09-03 15:18:22 -0400

    add link to shrimp store on brumpo store 🦐🦐🦐🦐🦐🦐🦐🦐🦐

--- src/Pages/Store/index.js
+++ src/Pages/Store/index.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './style.scss';
 
 const Store = () => {
+  const brumpoStoreLink =
+    'https://brumpo.myspreadshop.com/shrimp?collection=NFgmcBuNir&ref=friendshrimp';
   return (
     <div className="store">
       <h2>Shrimp Store stuff</h2>
       <div className="store__content">
-        <marquee className="small">
-          Where my shrimps?
-        </marquee>
+        <marquee className="small">Where my shrimps?</marquee>
       </div>
-      <div>
-        <h3>The Rest of the Store Coming Soon!</h3>
-        <p><em>The shrimp store is coming soon, more one of a kind shrimp items for sure</em></p>
+      <div className="visit-store">
+        <h3>Friendshrimp Merch on Brumpo Store</h3>
+        <p>Friendshrimp shrimp shrimp items :)</p>
+        <div>
+          <a
+            href={brumpoStoreLink}
+            className="button button--visit-store store-link"
+            title="Go For a Visit to the Friendshrimp Store"
+            target="_blank"
+            rel="nofollow"
+          >
+            Go to Friendshrimp Store
+          </a>
+        </div>
       </div>
     </div>
   );
